7fbd06df9053174ccc9b8203321504142ef7f0ea
may-h <hms9210@naver.com>
2021-03-15 19:13:42 +0900

    complete quiz!!!

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -18,7 +18,18 @@
     reason: string;
   };
 
+  // union type 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  // state라는 공통 프로퍼티를 사용하여 함수를 작성한다. 
+  const printLoginState = (loginState: ResourceLoadState): void => {
+    switch(loginState.state) {
+      case "loading": console.log(`👀 ${loginState.state}...`); break;
+      case "success": console.log(`😃 ${loginState.response.body}`); break;
+      case "fail": console.log(`😱 ${loginState.reason}`); break;
+      default: throw new Error('unknown State Type');
+    }
+  }
 
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
