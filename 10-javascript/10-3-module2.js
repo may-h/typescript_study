baff310a437ab390c8691c347924e55f8f4c1435
may-h <hms9210@naver.com>
2021-04-16 14:16:42 +0900

    learning typescript compile setting

--- 10-javascript/10-3-module2.js
+++ 10-javascript/10-3-module2.js
@@ -1,5 +1,5 @@
 //import run, {print} from './10-3-module1.js';
-import * as calculator from './10-3-module1.js';
+import * as calculator from './10-3-module1.js'; 
 console.log(calculator.add(1,2));
 calculator.print()
 
